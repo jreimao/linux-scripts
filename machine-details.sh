607887218b808ef1038d01261120ac27068e0508
joao reimao <jreimao@yahoo.com>
2023-06-15 18:24:50 +0100

    Edited machine-details.sh file

--- machine-details.sh
+++ machine-details.sh
@@ -2,7 +2,7 @@
 
 # author: joao reimao
 #
-# description: 
+# description: script to search and display machine details
 #
 # use: ./machine-details.sh --help
 #      ./machine-details.sh
@@ -10,7 +10,8 @@
 
 # help with short description and script execution syntax
 help () {
-        echo ""
+	echo
+        echo "script to search and display machine details"
         echo
         echo "$0 --help"
         echo "$0"
@@ -40,8 +41,38 @@ logs () {
 machine-details () {
         #
 
-}
+	KERNEL=$(uname -r)
+	HOSTNAME=$(hostname)
+	CPUNO=$(cat /proc/cpuinfo | grep 'model name' | wc -l)
+	CPUMODEL=$(cat /proc/cpuinfo | grep 'model name' | head -n1 | cut -c14-)
+	MEMTOTAL=$(expr $(cat /proc/meminfo | grep 'MemTotal' | tr -d ' ' | cut -d":" -f2 | tr -d 'kB') / 1024) # Em MB
+	FILESYS=$(df -h | egrep -v '(tmpfs|udev)')
+	UPTIME=$(uptime -s)
 
+	clear
+	echo
+	echo "report"
+	echo "date|time:  $(date)"
+	echo
+	echo "------------------------------------------------------------------"
+	echo
+	echo "computer: $HOSTNAME"
+	echo "machine active since: $UPTIME"
+	echo
+	echo "kernel version: $KERNEL"
+	echo
+	echo "cpu"
+	echo -e "\tcpu model: \t $CPUMODEL"
+	echo -e "\tcpu|core number: $CPUNO"
+	echo
+	echo "memory: $MEMTOTAL MB"
+	echo
+	echo "partitions:"
+	echo "$FILESYS"
+	echo
+	echo "------------------------------------------------------------------"
+	echo
+}
 
 # start of the script
 case $1 in
